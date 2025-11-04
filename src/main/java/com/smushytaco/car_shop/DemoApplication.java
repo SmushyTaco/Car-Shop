1858f028649a473125eee922db97b960d37e88e1
Nikan Radan <github.com@alias.nikanradan.com>
2025-11-03 23:49:37 -0800

    Java 25

--- src/main/java/com/smushytaco/car_shop/DemoApplication.java
+++ src/main/java/com/smushytaco/car_shop/DemoApplication.java
@@ -4,4 +4,4 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication { public static void main(final String[] args) { SpringApplication.run(DemoApplication.class, args); } }
+public class DemoApplication { static void main(final String[] args) { SpringApplication.run(DemoApplication.class, args); } }
